a011fba89d4c96b10b0d9b19045fa44ec6d5bfa8
Saobangmath <anhtai2672000@gmail.com>
2022-06-04 10:40:26 +0700

    async-await refactor

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,15 +3,22 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+const {interface,  bytecode} = require('../compile');
+
+let accounts, inbox;
 
 // fetch all eth accounts from ganache;
-beforeEach(() => {
-    web3.eth.getAccounts()
-        .then((accounts) => {
-            console.log(accounts);
-        });
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();    
+    // use one account to deploy the contract
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+                              .deploy({data : bytecode, arguments : ['HelloWorld']})
+                              .send({from: accounts[0], gas : '1000000'}); 
 });
 
 describe('Inbox', () => {
-    it('Deploy a contract', () =>{});
+    it('Deploy a contract', () =>{
+        console.log(inbox);
+    });
 })
